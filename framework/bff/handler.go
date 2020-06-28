1dfe0ac8df74a3336db95737700f603c2fb3790c
huarong.zheng <huarong.zheng@geekymedic.cn>
2020-06-28 14:57:11 +0800

    :fire:

--- framework/bff/handler.go
+++ framework/bff/handler.go
@@ -5,5 +5,7 @@ import "github.com/gin-gonic/gin"
 type BbfHandler func(state *State)
 
 func HttpHandler(handler BbfHandler) gin.HandlerFunc {
-	return HttpHandler(handler)
+	return func(ctx *gin.Context) {
+		handler(NewState(ctx))
+	}
 }
